de7d7c16adcf82a89bdf4f6b138bae058debf16c
xblueoceanx <ginnnokizuna@yahoo.co.jp>
2020-07-27 19:45:45 +0900

    [Add]エラーファイル追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -33,8 +33,13 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book)
+    else
+      @book = book
+      render :edit
+    end
   end
 
   def destroy
